f88af69ddb6e2a3bba7ebc3e11a6144546607427
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-07-20 20:39:31 +0100

    Update src/setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,30 +1,38 @@
 """Module setup.py"""
-import config
 import src.functions.directories
 
 
 class Setup:
     """
-
     Description
     -----------
 
     Sets up local & cloud environments
     """
 
-    def __init__(self):
-        """
-        Constructor
+    def __init__(self, warehouse: str):
         """
 
-        self.__configurations = config.Config()
+        :param warehouse: The temporary local directory where data sets are initially placed,
+                          prior to transfer to Amazon S3 (Simple Storage Service)
+        """
+
+        self.__warehouse = warehouse
 
     def __local(self):
+        """
+
+        :return:
+        """
 
         directories = src.functions.directories.Directories()
-        directories.cleanup(path=self.__configurations.warehouse)
-        directories.create(path=self.__configurations.warehouse)
+        directories.cleanup(path=self.__warehouse)
+        directories.create(path=self.__warehouse)
 
     def exc(self):
+        """
+
+        :return:
+        """
 
         self.__local()
