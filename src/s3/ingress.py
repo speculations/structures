5997f1e591f9237a0abf574bb03855d484e9e836
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-10 19:39:36 +0100

    Update src/s3/ingress.py

--- src/s3/ingress.py
+++ src/s3/ingress.py
@@ -1,11 +1,10 @@
-"""
-Module ingress.py
-"""
-import glob
-import os
+"""Module ingress.py"""
+import logging
+import urllib.parse
 
 import botocore.exceptions
 import dask
+import pandas as pd
 
 import src.elements.service as sr
 
@@ -20,49 +19,57 @@ class Ingress:
     Uploads files to Amazon Simple Storage Service (S3)
     """
 
-    def __init__(self, service: sr.Service, bucket_name, metadata: dict):
+    def __init__(self, service: sr.Service, bucket_name):
         """
 
         :param service: A suite of services for interacting with Amazon Web Services.
         :param bucket_name: The name of the target bucket.
-        :param metadata: The metadata of the files being uploaded. Note, files of the same content type are expected,
-                         assumed.
         """
 
         self.__s3_client = service.s3_client
         self.__bucket_name = bucket_name
-        self.__metadata = metadata
+
+        # Logging
+        logging.basicConfig(level=logging.INFO,
+                            format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.__logger = logging.getLogger(__name__)
 
     @dask.delayed
-    def __ingress(self, file: str, key: str) -> str:
+    def __ingress(self, file: str, key: str, metadata: dict) -> str:
         """
+        https://boto3.amazonaws.com/v1/documentation/api/latest/reference/customizations/
+            s3.html#boto3.s3.transfer.S3Transfer.ALLOWED_UPLOAD_ARGS
 
-        :param file: The local file string, i.e., <path> + <file name> + <extension>, of the file being uploaded
-        :param key: The Amazon S3 key of the file being uploaded; the key is relative-to the S3 Bucket name, but excludes
-                    the S3 Bucket name.
+        :param file: The local file string, i.e., <path> + <file name> + <extension>,
+                     of the file being uploaded
+        :param key: The Amazon S3 key of the file being uploaded; the key is
+                    relative-to the S3 Bucket name, but excludes the S3 Bucket name.
+        :param metadata: The metadata of the files being uploaded. Note, files of
+                         the same content type are expected, assumed.
         :return:
         """
 
+        tags = {key: value for key, value in metadata.items()}
+        tagging = urllib.parse.urlencode(tags)
+
         try:
             self.__s3_client.upload_file(Filename=file, Bucket=self.__bucket_name, Key=key,
-                                         ExtraArgs={'Metadata': self.__metadata})
+                                         ExtraArgs={'Metadata': metadata, 'Tagging': tagging})
             return f'Uploading {key}'
         except botocore.exceptions.ClientError as err:
             raise err from err
 
-    def exc(self, path: str) -> list[str]:
+    def exc(self, strings: pd.DataFrame) -> list[str]:
         """
 
-        :param path: The path to the files being transferred to Amazon Simple Storage Service (S3)
+        :param strings: The strings for Amazon Simple Storage Service (S3) transfers
         :return:
         """
 
-        files: list[str] = glob.glob(pathname=os.path.join(path, '**',  '*.csv'), recursive=True)
-        keys = [file.split(self.__bucket_name)[1].replace(os.path.sep, '/')[1:] for file in files]
-
         computations = []
-        for file, key in zip(files, keys):
-            message = self.__ingress(file=file, key=key)
+        for string in strings.to_dict(orient='records'):
+            message = self.__ingress(file=string['file'], key=string['key'], metadata=string['metadata'])
             computations.append(message)
         messages = dask.compute(computations, scheduler='threads')[0]
 
