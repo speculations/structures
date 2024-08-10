dd7b65a9085a41e5d8e6b8469cc2496269876c9e
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-08-10 21:49:33 +0100

    Update src/main.py: In Progress

--- src/main.py
+++ src/main.py
@@ -24,6 +24,7 @@ def main() -> None:
         src.data.source.Source(
             warehouse=configurations.warehouse).exc()
 
+
     # Delete Cache Points
     src.functions.cache.Cache().exc()
 
@@ -43,7 +44,6 @@ if __name__ == '__main__':
     # Modules
     import config
     import src.data.source
-    import src.data.dictionary
     import src.elements.s3_parameters as s3p
     import src.elements.service as sr
 
